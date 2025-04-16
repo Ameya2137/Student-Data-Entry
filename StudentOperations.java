c73bc41b77cd2ed7800189b2d0a2e3b4f5a5efbe
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-16 15:33:33 +0530

    Update StudentOperations.java by defining StudentOperations

--- StudentOperations.java
+++ StudentOperations.java
@@ -4,3 +4,8 @@ import java.util.*; // Importing the utility package for using ArrayList and oth
 
 public class StudentOperations {
     private ArrayList<Student> students; // List to store Student objects
+
+    // Constructor initializes the ArrayList
+    public StudentOperations() {
+        this.students = new ArrayList<>();
+    }
