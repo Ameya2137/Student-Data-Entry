93984a701b6f972c1986c5067f1ad6e3795cafdb
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-16 15:11:36 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+// Main.java
+
+// Name: Ameya Tipnis
+// PRN: 23070126012
+// Dept: SY-AIML-A1
+
+import java.util.*; // Importing utility classes like Scanner and Exception types
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); // Scanner object for input
+        StudentOperations operations = new StudentOperations();
+        int choice;
